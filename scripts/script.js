d85092c33b1dfe46e78ca0ed698e4f178bb6ec5f
Abdur Rahim <rahim11500000@gmail.com>
2024-02-18 12:55:38 +0600

    added coupon functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -29,6 +29,14 @@ function grandTotal() {
     const grandTotalText = document.getElementById("grand-total");
     const totalPrice = document.getElementById("total-price");
     grandTotalText.innerText = parseInt(totalPrice.innerText);
+    const couponValue = document.getElementById("coupon-value");
+    if (couponValue.value === "NEW15") {
+        const int = grandTotalText.innerText = parseInt(totalPrice.innerText) * .85;
+        console.log(int)
+    } else if(couponValue.value === "Couple 20"){
+        const int = grandTotalText.innerText = parseInt(totalPrice.innerText) * .80;
+        console.log(int)
+    }
     // console.log( parseInt(totalPrice.innerText));
     // console.log(grandTotalText.innerText)
 }
@@ -39,7 +47,7 @@ const arr = [];
 for (const seat of seats) {
     seat.addEventListener("click", function () {
         const totalSelectedSeats = document.getElementById("total-selected-seats");
-        
+
         if (parseInt(totalSelectedSeats.innerText) < 4 && !arr.includes(seat)) {
             const totalPrice = document.getElementById("total-price");
             const seatsLeft = document.getElementById("seats-left");
@@ -50,7 +58,26 @@ for (const seat of seats) {
             addBackgroundColor(seat);
             grandTotal();
             arr.push(seat)
-            
+
         }
     })
 }
+
+
+// coupon functionalities
+
+// coupon maching
+function couponMaching() {
+    // const couponValue = document.getElementById("coupon-value");
+    grandTotal()
+    // if (couponValue.value === "NEW15") {
+    //     return "NEW15"
+    // } else if (couponValue.value === "Couple 20") {
+    //     return "Couple 20"
+    // }
+}
+
+document.getElementById("coupon-button").addEventListener("click", couponMaching);
+
+// newsletter functionalities
+
